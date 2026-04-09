d8f7cb7fefe8740cd9e69c7ad16c353025495478
heymanou <alvarolucillore@gmail.com>
2026-04-10 01:22:15 +0200

    Add ext_repo.sh with ASCII art output

---
+++ ext_repo.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cat << 'EOF'
+STRINGLN      :,....  .....'   '';
+STRINGLN      ,'............  ,'''
+STRINGLN      ;'............',..'':
+STRINGLN      ;'.................''
+STRINGLN      ;'.................''
+STRINGLN      ,'..................'
+STRINGLN      ...............'''..,
+STRINGLN    :;,'',;:::::,''...,cl:;
+STRINGLN  ,.,;;;...',;;'.;ccc:..;
+STRINGLN .'ldxkkk;...'..oxkkO0O..,
+STRINGLN;.cOOOOkkc.....'00OOkkx..''',
+STRINGLN',oxxddc'. ....;odddl'.':,;
+STRINGLN  ;.'''.....'..'.......,
+STRINGLN     ;,,,,;ooddxl:;;:ccl
+STRINGLN      oc:cdkkOOOOko:clo
+STRINGLN      OkoxkkkkkkOOOkox0
+STRINGLN      OOOOOOkkkkkkOO000
+STRINGLN      OOOOOOkkkkkkkOO00
+STRINGLN      OOOOOOkkkkkkkOOOl
+STRINGLN      OOOOOOkkkkkkkkOOO0c:;;,,,',',:
+STRINGLN     OOOOOOkkkkkkkkkkkxx............'
+STRINGLN     kOOOOOkkkkkkkkkxxl' .............'
+STRINGLN    ,',xOOOkkkkkkkkk'...................
+STRINGLN  :..;kOOOOkkkkkkkkkk,..................
+STRINGLN :...xOOOOOkkkkkkkkkkd..................
+EOF
